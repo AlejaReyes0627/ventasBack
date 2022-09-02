af225ea691650fb54efe578da6b157c838a8c3f0
AlejaReyes0627 <aleja.reyes0627@hotmail.com>
2022-09-02 10:31:51 -0500

    Sesion 8 y 9

--- src/main/java/com/ventas/ventas/VentasApplication.java
+++ src/main/java/com/ventas/ventas/VentasApplication.java
@@ -1,12 +1,20 @@
 package com.ventas.ventas;
 
+import org.slf4j.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VentasApplication {
 
+	private VentasApplication ventasApplication;
+	private static final Logger logger= LoggerFactory.getLogger(VentasApplication.class);
 	public static void main(String[] args) {
+		logger.error("Mi primer log xd");
+		logger.trace("Mensaje de rastreo");
+		logger.debug("mensaje de depuracion");
+		logger.info("mensaje informacion");
+		logger.warn("mensaje advertencia");
 		SpringApplication.run(VentasApplication.class, args);
 	}
 
